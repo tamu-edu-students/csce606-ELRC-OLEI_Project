ddbb52d0782f6a5a7c1b6dd94e657ea21838076a
Sai Aakarsh Padma <saiaakarshpadma@Sais-MacBook-Air.local>
2024-10-04 19:03:32 -0500

    Rendering survey responses in admin dashboard page

--- rails_root/app/controllers/admins_controller.rb
+++ rails_root/app/controllers/admins_controller.rb
@@ -1,4 +1,6 @@
+#admins_controller.rb
 class AdminsController < ApplicationController
   def index
+    @survey_responses = SurveyResponse.get_all_responses(page: params[:page], per_page: 20)
   end
 end
