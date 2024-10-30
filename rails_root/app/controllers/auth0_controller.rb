bfbc2a1a8d73cf6c68718a2e923e000fbd478c33
GURRAM MANOJ REDDY <manoj850@tamu.edu>
2024-10-29 23:39:32 -0500

    Admin Integration -1

--- rails_root/app/controllers/auth0_controller.rb
+++ rails_root/app/controllers/auth0_controller.rb
@@ -3,32 +3,21 @@
 # ./app/controllers/auth0_controller.rb
 class Auth0Controller < ApplicationController
   def callback
-    # OmniAuth stores the information returned from Auth0 and the IdP in request.env['omniauth.auth'].
-    # In this code, you will pull the raw_info supplied from the id_token and assign it to the session.
-    # Refer to https://github.com/auth0/omniauth-auth0/blob/master/EXAMPLES.md#example-of-the-resulting-authentication-hash for complete information on 'omniauth.auth' contents.
-
     auth_info = request.env['omniauth.auth']
-    # puts JSON.pretty_generate(auth_info)
+    user_roles = auth_info['extra']['raw_info']['https://myapp.com/123456789012/roles/roles']
     session[:userinfo] = auth_info['extra']['raw_info']
-
-    # print session info in pretty JSON format
-    # puts JSON.pretty_generate(session[:userinfo])
-
-    # create new survey profile if the user is 'new'
-    # if no survey profile contains unique user_id, create a new survey profile
-    # puts 'trying to find survey profile'
-    # puts SurveyProfile.find_by(user_id: session[:userinfo]['sub'])
-
-    if SurveyProfile.find_by(user_id: session[:userinfo]['sub']).nil?
+  
+    # Check if the user has an Admin role
+    if user_roles && user_roles.include?('Admin')
+      redirect_to admin_dashboard_path  # Changed to admin_dashboard_path
+    elsif SurveyProfile.find_by(user_id: session[:userinfo]['sub']).nil?
       redirect_to new_survey_profile_path
     elsif session[:invitation] && claim_invitation
       # Nothing to do here, claim_invitation already did the redirect
     else
-      # Redirect to the URL you want after successful auth
       redirect_to root_url
     end
   end
-
   def failure
     # Handles failed authentication -- Show a failure page (you can also handle with a redirect)
     @error_msg = request.params['message']
